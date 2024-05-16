6ae1576da1c454a22791f0cc67240f0f46dde1ad
Divyansh Dev <92811456+devdivyansh522399@users.noreply.github.com>
2024-05-16 21:40:19 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://backend-mycare-a-healthcare-app.onrender.com'
+export const BASE_URL = 'https://backend-mycare-a-healthcare-app.onrender.com/api/v1'
 export const token = localStorage.getItem('token');
 export let user = localStorage.getItem('user');
 export const role = localStorage.getItem('role')
